8e5519e3d7172de031d8a6955d8e9acd50c95a17
Alameen007 <alaminabdulkareem@gmail.com>
2019-09-25 12:29:37 +0100

    Build a mini project using TypeScipt

---
+++ src/components/Scroll.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+
+type props = {
+  children?: JSX.Element
+}
+
+const Scroll = (props: props) => {
+  return (
+    <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
